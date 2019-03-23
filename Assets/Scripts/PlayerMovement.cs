c9da69cfac55fb21f5c7fbece0db8fdff3aaeb55
Brooke Stewart <brooke44@live.co.za>
2019-03-23 16:43:15 +0200

    Player spits teeth in correct direction (shoots bullets) but two fire on a single key press

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,40 +5,64 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed =3;
-    public GameObject playerSkeleton;
+    public GameObject PlayerSkeleton;
     List<GameObject> players = new List<GameObject>(2);
+
+    private Vector3 P1direction = new Vector3(0,-1,0);
+    float lastShotP1 =0f;
+    private Vector3 P2direction=new Vector3(0,-1,0);
+
+    //Variables for shooting
+    public GameObject ToothShot; //prefab
+    public GameObject ToothShotOrigin; //from whence the tooth is spat
+
+    //private int P1bullet;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+         //add both players to the List of players
+        foreach(GameObject player in GameObject.FindObjectsOfType (typeof(GameObject))){
+            if(player.tag=="Player"&&!players.Contains(player))
+                players.Add(player);
+        }
+        //P1bullet=1;
     }
     
     // Update is called once per frame
     void Update()
     {
-        //add both players to the List of players
-        foreach(GameObject player in GameObject.FindObjectsOfType (typeof(GameObject))){
-            if(player.tag=="Player"&&!players.Contains(player))
-                players.Add(player);
-        }
-
         //Assign Arrow keys for player 1
         if(Input.GetKey(KeyCode.LeftArrow)){
             players[0].transform.rotation = Quaternion.LookRotation(players[0].transform.forward,Vector2.right);
-            players[0].transform.position += (new Vector3(-1,0,0))*Time.deltaTime*speed;
+            P1direction=new Vector3(-1,0,0);
+            players[0].transform.position+=P1direction*speed*Time.deltaTime;
         }
         if(Input.GetKey(KeyCode.RightArrow)){
             players[0].transform.rotation=Quaternion.LookRotation(players[0].transform.forward,Vector2.left);
-            players[0].transform.position += (new Vector3(1,0,0))*Time.deltaTime*speed;
+            P1direction=new Vector3(1,0,0);
+            players[0].transform.position+=P1direction*speed*Time.deltaTime;
         }
         if(Input.GetKey(KeyCode.UpArrow)){
             players[0].transform.rotation=Quaternion.LookRotation(players[0].transform.forward,Vector2.down);
-            players[0].transform.position += (new Vector3(0,1,0))*Time.deltaTime*speed;
+            P1direction=new Vector3(0,1,0);
+            players[0].transform.position+=P1direction*speed*Time.deltaTime;
         } 
         if(Input.GetKey(KeyCode.DownArrow)){
             players[0].transform.rotation=Quaternion.LookRotation(players[0].transform.forward,Vector2.up);
-            players[0].transform.position += (new Vector3(0,-1,0))*Time.deltaTime*speed;
+            P1direction=new Vector3(0,-1,0);
+            players[0].transform.position+=P1direction*speed*Time.deltaTime;
+        }
+        //for player 1 fire tooth(bullet) when enter pressed
+        if(Input.GetKeyDown(KeyCode.M)){
+            
+            //instantiate tooth projectile
+            if(lastShotP1!=Time.time){
+                GameObject toothBullet = (GameObject)Instantiate(ToothShot);
+                toothBullet.transform.position = players[0].transform.GetChild(0).transform.position;
+                toothBullet.SendMessage("theDirection", P1direction);
+                lastShotP1=Time.time;
+            }
         }
 
         //Assign WASD keys for player 2
@@ -58,6 +82,9 @@ public class PlayerMovement : MonoBehaviour
             players[1].transform.rotation=Quaternion.LookRotation(players[0].transform.forward,Vector2.up);
             players[1].transform.position += (new Vector3(0,-1,0))*Time.deltaTime*speed;
         }
+        //for player 2 fire tooth(bullet) when f is pressed
 
+
+        //P1bullet=0;
     }
 }
---
+++ Assets/Scripts/ToothShotScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothShotScript : MonoBehaviour
+{
+    float speed;
+    // Start is called before the first frame update
+    private Vector3 direction;
+
+    //get the direction the player object is facing
+    void theDirection(Vector3 direction){
+        this.direction=direction;
+    }
+    void Start()
+    {
+        //theDirection(new Vector2(0,1)); testing
+        speed = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current position
+        Vector3 position = transform.position;
+
+        //move tooth(bullet)
+        Vector3 newPosition = position + direction*speed*Time.deltaTime;
+        transform.position=newPosition;
+
+    }
+
+}
