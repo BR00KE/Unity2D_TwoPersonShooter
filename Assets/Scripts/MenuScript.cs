9505bec8c2306675e6348da69856bc8005be067b
Brooke Stewart <brooke44@live.co.za>
2019-03-23 23:07:43 +0200

    Menu scene added. Level1 button and quit button work

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void playLevelOne()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);   
+    }
+
+    public void playeLevelTwo()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); 
+    }
+    public void playLevelThree()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); 
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
