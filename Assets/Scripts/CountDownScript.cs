84b0191e9fe1b7de777fe6c26092de9688835b16
Brooke Stewart <brooke44@live.co.za>
2019-03-24 19:18:12 +0200

    level time limit added to level 1

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour
+{
+    //public float timeLimit;
+    public Text TimeRemainingText;
+    public Text endText;
+
+    public float levelTimer;
+
+    private bool levelended;
+
+    List<GameObject> players = new List<GameObject>(2);
+    void Start()
+    {
+        levelended=false;
+
+        foreach(GameObject player in GameObject.FindObjectsOfType (typeof(GameObject))){
+            if(player.tag=="Player"&&!players.Contains(player))
+                players.Add(player);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currentTime=TimeRemainingText.time;
+        levelTimer-=Time.deltaTime;
+
+        if(levelTimer>0){
+            if(!levelended){
+                float minutes = (levelTimer / 60);
+                float seconds = (levelTimer % 60);
+                TimeRemainingText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+                //TimeRemainingText.text = "Time Remaining: "+(levelTimer);
+            }
+        }
+        else{
+            //time is up end scene
+            levelended=true;
+            StartCoroutine("callToEndSceneFailed");
+        }
+    }
+    IEnumerator callToEndSceneFailed(){
+        //disable players
+        players[1].SetActive(false);
+        players[0].SetActive(false);
+
+        Debug.Log("got to wait");
+        endText.text = "LEVEL FAILED";
+        yield return new WaitForSeconds(4);
+        FindObjectOfType<SceneManagementScript>().endScene();
+    }
+}
--- Assets/Scripts/KillCountScript.cs
+++ Assets/Scripts/KillCountScript.cs
@@ -10,11 +10,19 @@ public class KillCountScript : MonoBehaviour
     public Text KillCountText;
     public Text endText;
     private float killCount;
+
+    List<GameObject> players = new List<GameObject>(2);
     // Start is called before the first frame update
     void Start()
     {
         killCount=0f;
         setKillCountDisplay();
+
+        //find player objects
+        foreach(GameObject player in GameObject.FindObjectsOfType (typeof(GameObject))){
+            if(player.tag=="Player"&&!players.Contains(player))
+                players.Add(player);
+        }
     }
 
     void Update(){
@@ -24,17 +32,21 @@ public class KillCountScript : MonoBehaviour
         killCount+=0.5f;
         setKillCountDisplay();
 
-        if(Mathf.Ceil(killCount)>=10){
+        if(Mathf.Ceil(killCount)>=15){
             Debug.Log("end condition");
             StartCoroutine("callToEndSceneSuccess");
         }
     }
 
     void setKillCountDisplay(){
-        KillCountText.text = "Spiders Killed: "+Mathf.Ceil(killCount).ToString()+"/10";
+        KillCountText.text = "Spiders Killed: "+Mathf.Ceil(killCount).ToString()+"/15";
     }
 
     IEnumerator callToEndSceneSuccess(){
+        //disable player objects
+        players[1].SetActive(false);
+        players[0].SetActive(false);
+
         Debug.Log("got to wait");
         endText.text = "LEVEL PASSED";
         yield return new WaitForSeconds(4);
